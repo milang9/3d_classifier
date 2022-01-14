03749c79df12ce6df03e266f2521a48a29c3635a
milang9 <milan.geyer@gmx.at>
2022-01-14 10:13:27 +0100

    oris

--- scripts/copy_originials.py
+++ scripts/copy_originials.py
@@ -5,8 +5,10 @@ import sys
 
 file_path = sys.argv[1]
 
+directory = sys.argv[2]
+
 for file in os.listdir(file_path):
     if "be0" in file:
         cg = file[:-9]
         ori = cg + "001.cg"
-        shutil.copy("/scr/risa/mgeyer/ernwinenv/data/test_set_5st/" + ori, file_path + ori)
+        shutil.copy("/scr/risa/mgeyer/ernwinenv/data/" + directory + ori, file_path + ori)
