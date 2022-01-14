1127f3577e6d926980d57c30bf3ee3902307c3e2
milang9 <milan.geyer@gmx.at>
2022-01-14 10:31:14 +0100

    getting original structures

--- scripts/copy_originials.py
+++ scripts/copy_originials.py
@@ -10,5 +10,5 @@ directory = sys.argv[2]
 for file in os.listdir(file_path):
     if "be0" in file:
         cg = file[:-9]
-        ori = cg + "001.cg"
+        ori = cg + ".cg"
         shutil.copy("/scr/risa/mgeyer/ernwinenv/data/" + directory + ori, file_path + ori)
