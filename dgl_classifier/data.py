11d5441beb44c4cee7fa913c0717215695c9a81f
milang9 <milan.geyer@gmx.at>
2022-02-18 10:08:42 +0100

    reorganisation

---
+++ dgl_classifier/data.py
@@ -0,0 +1,120 @@
+import numpy as np
+import torch as th
+import forgi.threedee.model.coarse_grain as ftmc
+import os
+import dgl
+from dgl.data import DGLDataset
+
+#Graph Building
+#load coarse grain file
+def load_cg_file(file): 
+    cg = ftmc.CoarseGrainRNA.from_bg_file(file) 
+    c_dict = dict(cg.coords)
+    t_dict = dict(cg.twists)
+    coord_dict = {}
+    twist_dict = {}
+    for e in c_dict:
+        a = th.from_numpy(c_dict[e][0])
+        b = th.from_numpy(c_dict[e][1])
+        coord_dict[e] = a, b
+        if e in t_dict:
+            c = th.from_numpy(t_dict[e][0])
+            d = th.from_numpy(t_dict[e][1])
+            twist_dict[e] = c, d
+        
+    # Get elements and neighbours:
+    connections = {}
+    for elem in cg.sorted_element_iterator():
+        neighbours = cg.connections(elem)
+        if elem not in connections:
+            connections[elem] = cg.connections(elem)
+    return coord_dict, twist_dict, connections
+
+def build_dgl_graph(coord_dict, twist_dict, connections, device):
+    #dictionary to encode type
+    type_transl = {
+        "h": [1, 0, 0, 0, 0, 0],
+        "i": [0, 1, 0, 0, 0, 0],
+        "m": [0, 0, 1, 0, 0, 0],
+        "s": [0, 0, 0, 1, 0, 0],
+        "f": [0, 0, 0, 0, 1, 0],
+        "t": [0, 0, 0, 0, 0, 1]
+    } 
+
+    #encode nodes numerically for dgl graph
+    num_graph = {}
+    elem_count = {}
+    for num, n in enumerate(sorted(connections)):
+        num_graph[n] = num
+        if n[0] not in elem_count:
+            elem_count[n[0]] = 1
+        else:
+            elem_count[n[0]] += 1
+
+    #build graph and edges
+    u = []
+    v = []
+    for node in connections:
+        for c in connections[node]:
+            u.append(num_graph[node])
+            v.append(num_graph[c])
+
+    graph = dgl.graph((th.tensor(u), th.tensor(v)))
+
+    #initialise node attributes
+    graph.ndata["type"] = th.zeros(graph.num_nodes(), 6, dtype=th.float32)
+    graph.ndata["coord"] = th.zeros(graph.num_nodes(), 6, dtype=th.float32)
+    graph.ndata["twist"] = th.zeros(graph.num_nodes(), 6, dtype=th.float32)
+
+    for elem in connections:
+        graph.ndata["type"][num_graph[elem]] = th.tensor(type_transl[elem[0]], dtype=th.float32) 
+        graph.ndata["coord"][num_graph[elem]] = th.tensor(np.concatenate(coord_dict[elem]), dtype=th.float32)
+        if elem in twist_dict:
+            graph.ndata["twist"][num_graph[elem]] = th.tensor(np.concatenate(twist_dict[elem]), dtype=th.float32)
+    
+    return graph.to(device)
+
+#create a dict with name and rmsd as labels
+def get_rmsd_dict(rmsd_list):
+    rmsd_dict = {}
+    with open(rmsd_list, "r") as fh:
+        for line in fh.readlines():
+            name, rmsd = (line.rstrip()).split("\t")
+            rmsd_dict[name] = float(rmsd)
+    return rmsd_dict
+
+#Graph Dataset Class
+class CGDataset(DGLDataset):
+    def __init__(self, directory, rmsd_list, device):
+        self.file_path = directory
+        self.rmsd_list = rmsd_list
+        self.device = device
+        super(CGDataset, self).__init__(name="cgRNA")
+         
+    def process(self):
+        self.graphs = []
+        rmsd_dict = get_rmsd_dict(self.rmsd_list)
+        self.labels = []
+        
+        files = []
+        filenames = next(os.walk(self.file_path), (None, None, []))[2]
+
+        for file in filenames:
+            if file.endswith(".cg") and file in rmsd_dict.keys():
+                files.append(file)
+                self.labels.append(rmsd_dict[file])
+
+        for struc in files:
+            coord_dict, twist_dict, connections = load_cg_file(os.path.join(self.file_path, struc))
+            self.graphs.append(build_dgl_graph(coord_dict, twist_dict, connections, self.device))
+
+        self.labels = th.tensor(self.labels).to(self.device)
+  
+    def __getitem__(self, i):
+        return self.graphs[i], self.labels[i]
+
+    def __len__(self):
+        return len(self.graphs)
+    
+    #def save(self):
+     #   dgl.save_graphs("./play_set/training_cg_graphs.dgl", self.graphs, labels=self.labels)
---
+++ pyg_classifier/data.py
@@ -0,0 +1,134 @@
+import numpy as np
+import torch as th
+import forgi.threedee.model.coarse_grain as ftmc
+import os
+from torch_geometric.data import Data
+from torch_geometric.data import InMemoryDataset
+
+#Graph Building
+#load coarse grain file
+def load_cg_file(file): 
+    cg = ftmc.CoarseGrainRNA.from_bg_file(file) 
+    c_dict = dict(cg.coords)
+    t_dict = dict(cg.twists)
+    coord_dict = {}
+    twist_dict = {}
+    for e in c_dict:
+        a = th.from_numpy(c_dict[e][0])
+        b = th.from_numpy(c_dict[e][1])
+        coord_dict[e] = a, b
+        if e in t_dict:
+            c = th.from_numpy(t_dict[e][0])
+            d = th.from_numpy(t_dict[e][1])
+            twist_dict[e] = c, d
+        
+    # Get elements and neighbours:
+    connections = {}
+    for elem in cg.sorted_element_iterator():
+        neighbours = cg.connections(elem)
+        if elem not in connections:
+            connections[elem] = cg.connections(elem)
+    return coord_dict, twist_dict, connections
+
+#create a dict with name and rmsd as labels
+def get_rmsd_dict(rmsd_list):
+    rmsd_dict = {}
+    with open(rmsd_list, "r") as fh:
+        for line in fh.readlines():
+            name, rmsd = (line.rstrip()).split("\t")
+            rmsd_dict[name] = float(rmsd)
+    return rmsd_dict
+
+def build_graph(coord_dict, twist_dict, connections, label):
+    #dictionary to convert type
+    type_transl = {
+        "h": [1, 0, 0, 0, 0, 0],
+        "i": [0, 1, 0, 0, 0, 0],
+        "m": [0, 0, 1, 0, 0, 0],
+        "s": [0, 0, 0, 1, 0, 0],
+        "f": [0, 0, 0, 0, 1, 0],
+        "t": [0, 0, 0, 0, 0, 1]
+    } 
+
+    #encode nodes numerically for edge index
+    num_graph = {}
+    for num, n in enumerate(sorted(connections)):
+        num_graph[n] = num
+
+
+    #build graph and edges
+    u = []
+    v = []
+    for node in connections:
+        for c in connections[node]:
+            u.append(num_graph[node])
+            v.append(num_graph[c])
+    
+    edge_index = th.tensor([u, v], dtype=th.long)
+
+    x = []
+    for elem in sorted(connections):
+        a = np.array(type_transl[elem[0]])
+        b = np.concatenate(coord_dict[elem])
+        if elem in twist_dict:
+            c = np.concatenate(twist_dict[elem]) 
+        else:
+            c = np.zeros(6)
+        z = np.append(a, [b, c])
+        x.append(z)
+    x = np.array(x)
+    x = th.tensor(x, dtype=th.float32)
+
+    graph = Data(x=x, edge_index=edge_index, y=label)
+
+    return graph
+
+#Graph Dataset Class
+class CGDataset(InMemoryDataset):
+    def __init__(self, root, rmsd_list, transform=None, pre_transform=None):
+        self.file_path = root
+        self.rmsd_list = rmsd_list
+        super(CGDataset, self).__init__(root, transform, pre_transform)
+        self.data, self.slices = th.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        return [f for f in os.listdir(self.file_path) if os.path.isfile(os.path.join(self.file_path, f))]
+    
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
+
+    def process(self):
+        self.graphs = []
+        rmsd_dict = get_rmsd_dict(self.rmsd_list)
+        
+        files = []
+
+        for file in self.raw_file_names:
+            if file.endswith(".cg") and file in rmsd_dict.keys():
+                files.append(file)
+
+        for struc in files:
+            coord_dict, twist_dict, connections = load_cg_file(os.path.join(self.file_path, struc))
+            graph = build_graph(coord_dict, twist_dict, connections, rmsd_dict[struc])
+            self.graphs.append(graph)
+
+        if self.pre_filter is not None:
+            self.graphs = [data for data in self.graphs if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            self.graphs = [self.pre_transform(data) for data in self.graphs]
+
+        data, slices = self.collate(self.graphs)
+        th.save((data, slices), self.processed_paths[0])
+        
+    '''
+    def get(self, idx):
+        data = torch.load(osp.join(self.processed_dir, f'data_{idx}.pt'))
+        return data
+
+    def len(self):
+        return len(self.graphs)
+    '''
+
