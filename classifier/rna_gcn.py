2864b9499e63ef2f0a5ea055022c5ee641824b71
milang9 <milan.geyer@gmx.at>
2022-05-10 15:13:29 +0200

    command line training

--- classifier/rna_gcn.py
+++ classifier/rna_gcn.py
@@ -1,28 +1,62 @@
 #!/usr/bin/python
 import argparse
 import torch as th
-import classifier.train as ct #import pool_train_loop, add_train_specific_args
-import classifier.model as cm # import MinCut2_CG_Classifier
+from train import pool_train_loop, add_train_specific_args
+from model import CG_Classifier, Diff_CG_Classifier, MinCut_CG_Classifier, MinCut2_CG_Classifier
+from data import CGDataset
+import torch_geometric.transforms as T
 
-def main():
+def add_args():
     parser = argparse.ArgumentParser(description="Neural Network Model for the classification of coarse grained RNA 3D structures, using Graph Convolution.")
+    parser.add_argument("-model", default="mincut", const="mincut", nargs="?", choices=["tag", "diffpool", "mincut", "mincut2", "dmon"])
     parser.add_argument("-train", action="store_true")
     parser.add_argument("-test", action="store_true")
-    parser = ct.add_train_specific_args(parser)
+    parser.add_argument("-t", "--training_set")
+    parser.add_argument("-t_rmsd")
+    parser.add_argument("-v", "--val_set")
+    parser.add_argument("-v_rmsd")
+    parser.add_argument("-o", "--output_dir")
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("-lr", "--learning_rate", default=1e-3)
+    parser.add_argument("-epochs", type=int, default=100)
+    parser.add_argument("--cycle_length", type=int, default=0, help="Length of learning rate decline cycles.")
+    parser.add_argument("--vector", action="store_true", default=False, help="Transform coordinates into vector representation.")
+    parser.add_argument("-k", type=int, default=0, help="Use the start and end of the element vectors, pointing to the next k elements.")
+    parser.add_argument("-seed", type=int, default=None)
+    parser.add_argument("-burn_in", type=int, default=0)
+    return parser
+
+
+def main():
+    parser = add_args()
+    #parser = add_train_specific_args(parser)
     args = parser.parse_args()
     device = th.device("cuda" if th.cuda.is_available() else "cpu")
+    
+    if args.training_set:
+        training_dataset = CGDataset(args.training_set, args.t_rmsd, args.vector, args.k, transform=T.ToDense(64))
+    if args.val_set:
+        val_dataset = CGDataset(args.val_set, args.v_rmsd, args.vector, args.k, transform=T.ToDense(64))
 
 
+    if args.model == "tag":
+        m = CG_Classifier(training_dataset.num_node_features)
+    elif args.model == "diffpool":
+        m = Diff_CG_Classifier(training_dataset.num_node_features)
+    elif args.model == "mincut":
+        m = MinCut_CG_Classifier(training_dataset.num_node_features)
+    elif args.model == "mincut2":
+        m = MinCut2_CG_Classifier(training_dataset.num_node_features)
 
     if args.train:
-        model = cm.MinCut2_CG_Classifier
-        ct.pool_train_loop(
-            model=model,
-            train_dataset=args.t,
-            val_dataset=args.v,
+        pool_train_loop(
+            model=m,
+            train_dataset=training_dataset,
+            val_dataset=val_dataset,
+            model_dir=args.output_dir,
             device=device,
             b_size=args.batch_size,
-            lr=args.lr,
+            lr=args.learning_rate,
             epochs=args.epochs,
             sched_T0=args.cycle_length,
             vectorize=args.vector,
