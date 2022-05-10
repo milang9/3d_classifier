12804dd33dcf800384bff6baf43e7e67c0c5d0c6
milang9 <milan.geyer@gmx.at>
2022-05-10 11:17:18 +0200

    2022-05-10

--- classifier/rna_gcn.py
+++ classifier/rna_gcn.py
@@ -2,6 +2,7 @@
 import argparse
 import torch as th
 from classifier.train import pool_train_loop, add_train_specific_args
+from classifier.model import MinCut2_CG_Classifier
 
 def main():
     parser = argparse.ArgumentParser(description="Neural Network Model for the classification of coarse grained RNA 3D structures, using Graph Convolution.")
@@ -11,24 +12,24 @@ def main():
     args = parser.parse_args()
     device = th.device("cuda" if th.cuda.is_available() else "cpu")
 
-    
+
 
     if args.train:
-        model = ""
+        model = MinCut2_CG_Classifier
         pool_train_loop(
             model=model,
             train_dataset=args.t,
             val_dataset=args.v,
             device=device,
-            b_size=args.batch_size, 
+            b_size=args.batch_size,
             lr=args.lr,
-            epochs=args.epochs, 
+            epochs=args.epochs,
             sched_T0=args.cycle_length,
             vectorize=args.vector,
             k=args.k,
             seed=args.seed,
             burn_in=args.burn_in)
-    
+
     if args.test:
         pass
 
